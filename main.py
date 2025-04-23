2b98664208d01bc57c994e400932cf67070201c3
Noemiekt <noemie.kt@gmail.com>
2025-04-23 17:26:01 +0200

    ajout des fichiers qui contienne l'articulation l

--- main.py
+++ main.py
@@ -1,19 +1,17 @@
-# venv
-import pyaudio # type: ignore
-import pandas as pd # type: ignore
-import numpy as np 
-import joblib # type: ignore
-import matplotlib.pyplot as plt 
+# jouer sur les parametres de threshold de temps reel et correction
+import pyaudio 
+import pandas as pd  
+import numpy as np  
+import joblib  
 import librosa 
-from scipy.signal import butter, lfilter 
-import mido # type: ignore
-import fluidsynth # type: ignore
-import scripts.modif_libro.spectral as spectral     
+from scipy.signal import butter
+import mido  
+import fluidsynth  
+from enum import Enum
 
 ##########################################################################################
 # CHANGER LA SOURCE ET LA SORTIE 
 # pactl set-default-source alsa_input.usb-Focusrite_Scarlett_4i4_USB_D89YRE90C17918-00.multichannel-input
-# pactl set-default-sink alsa_output.pci-0000_00_1f.3.analog-stereo
 # pactl set-default-sink alsa_output.pci-0000_00_1f.3.analog-stereo
 
 ##########################################################################################
@@ -22,21 +20,24 @@ import scripts.modif_libro.spectral as spectral
 # Initialiser FluidSynth avec une soundfont
 fluid = fluidsynth.Synth()
 fluid.start()
-sfid = fluid.sfload("FluidR3_GM.sf2")  
+sfid = fluid.sfload("/usr/share/sounds/sf2/FluidR3_GM.sf2")  
 fluid.program_select(0, sfid, 0, 73)
 
 # Charger le fichier MIDI
-midi_file = mido.MidiFile("midi/frozen.mid")
+midi_file = mido.MidiFile("midi/potter.mid")
 
-# Charger le modèle
-knn_model = joblib.load("scripts/knn_model.pkl")
+# Charger le modele et les vecteurs propres
+knn_model = joblib.load("scripts/knn_model_db_sans_r_opt_main_corrige_avant.pkl")  
+eigenvectors_thresholded = joblib.load("scripts/eigenvectors_thresholded_corrige_avant_tronque.pkl") 
+mean_X = joblib.load("scripts/mfcc_mean.pkl")
+std_X = joblib.load("scripts/mfcc_std.pkl")
 
 # Paramètres audio
-filename = "audio/recorded.wav"
+taux_recouvrement = 1
 FORMAT = pyaudio.paInt16 
 CHANNELS = 1  
 RATE = 44100
-CHUNK = int(RATE * 0.005) 
+CHUNK = int((RATE * 0.005) // taux_recouvrement)
 
 # Initialisation de PyAudio
 p = pyaudio.PyAudio()
@@ -49,9 +50,6 @@ stream = p.open(
     channels=CHANNELS,
     rate=RATE,
     input=True,
-    output=True,
-    # input_device_index=1,
-    # output_device_index=1,
     frames_per_buffer=CHUNK
 )
 audio_frames = []
@@ -61,20 +59,28 @@ frames = []
 mfcc_features = []
 time_values = []
 start = 0
-n_fft = min(512, CHUNK)
-window = np.ones(min(512, CHUNK))
+n_fft = min(512, int(CHUNK * taux_recouvrement))
+window = np.ones(min(512, int(CHUNK * taux_recouvrement)))
 win_length = n_fft
 
 # Build a Mel filter
-mel_basis = librosa.filters.mel(sr=RATE, n_fft=n_fft, fmax=RATE/2, n_mels=10)
+mel_basis = librosa.filters.mel(sr=RATE, n_fft=n_fft, fmax=RATE/2, n_mels=40)
 
-batch_size = 10  # Nombre de blocs MFCC à accumuler avant prédiction
+# Calcul FFT
+fft_window = librosa.filters.get_window(window, win_length, fftbins=True)
+
+# Pad the window out to n_fft size
+fft_window = librosa.util.pad_center(fft_window, size=n_fft)
+
+batch_size = 6  # Nombre de blocs MFCC à accumuler avant prédiction
 mfcc_buffer = []
 tab_pred = []
 events = []
+proba_list = []
 
 # Stocker la dernière prédiction pour comparaison
 last_prediction = None
+last_prediction_boucle = None
 
 ##########################################################################################
 # DEFINITION DU FILTRE PASSE BAS
@@ -84,59 +90,178 @@ order=1
 normal_cutoff = cutoff / nyquist
 b, a = butter(order, normal_cutoff, btype="low", analog=False)
 
+##########################################################################################
+# AUTOMATE
+# Dictionnaire des transitions
+transitions = {
+    ("1", "l"): "5",  ("1", "a"): "4",  ("1", "t"): "3",  ("1", "s"): "2",
+    ("2", "l"): "5",  ("2", "a"): "4",  ("2", "t"): "3",  ("2", "s"): "10",
+    ("3", "l"): "6",  ("3", "a"): "9",  ("3", "t"): "6",  ("3", "s"): "2",
+    ("4", "l"): "7",  ("4", "a"): "9",  ("4", "t"): "6",  ("4", "s"): "2",
+    ("5", "l"): "5",  ("5", "a"): "4",  ("5", "t"): "3",  ("5", "s"): "10",
+    ("6", "l"): "6",  ("6", "a"): "9",  ("6", "t"): "6",  ("6", "s"): "2",
+    ("7", "l"): "7",  ("7", "a"): "8",  ("7", "t"): "7",  ("7", "s"): "2",
+    ("8", "l"): "7",  ("8", "a"): "9",  ("8", "t"): "9",  ("8", "s"): "2",
+    ("9", "l"): "7",  ("9", "a"): "9",  ("9", "t"): "9",  ("9", "s"): "2",
+    ("10", "l"): "5", ("10", "a"): "4", ("10", "t"): "3", ("10", "s"): "10"
+}
+
+# Fonction de transition qui retourne le nouvel état et l'action associée
+def transition(etat, char):
+    new_state = transitions.get((etat, char), etat) 
+    action = None
+
+    if etat == "2":
+        action = "OFF"
+    elif etat in {"3", "4"}:
+        action = "ON"
+    elif etat == "8":
+        action = "ON_OFF"
+
+    return new_state, action
+
+# NOTE_ON AND NOTE_OFF
+# Initialisation de la pile
+note_stack = []
+note_pointer = 0  # Pour suivre la position dans midi_notes
+
+# Fonction pour traiter un événement ON ou OFF
+def handle_event(event_type):
+    global note_pointer
+    if event_type == "ON":
+        if note_pointer < len(midi_notes):
+            note = midi_notes[note_pointer]
+            note_stack.append(note)
+            fluid.noteon(0, note.note, note.velocity)
+            # print(f"ON → note {note.note} (pile = {[n.note for n in note_stack]})")
+            note_pointer += 1
+    elif event_type == "OFF":
+        if note_stack:
+            note_to_off = note_stack.pop(0)
+            fluid.noteoff(0, note_to_off.note)
+            # print(f"OFF → note {note_to_off.note} (pile = {[n.note for n in note_stack]})")
 
 ##########################################################################################
 # PRISE DU FLUX AUDIO EN TEMPS REEL
-etat = None
-midi_notes = [msg for msg in midi_file if msg.type == 'note_on' and msg.velocity !=0]   
+prev_event = None
+midi_notes = [msg for msg in midi_file if msg.type == 'note_on' and msg.velocity !=0]  # Extraire les notes MIDI
 note_id = -1
+buffer_sensibilite_l = []
+buffer_sensibilite_a = []
+audio_buffer = np.array([], dtype=np.int16)
 
+class Label(Enum):
+    L = 0
+    A = 1
+    S = 2
+    T = 3
+    R = 4
+    VIDE = 5
+
+class Event(Enum):
+    ON_NOTE_T = 0
+    OFF_S = 1
+    ON_L = 2
+    ON_NOTE_L = 3
+    OFF_R = 4
+    
+# Chargement du jeu d'entraînement pour obtenir les points 'l'
+label_mapping = {"l": 0, "a": 1, "s": 2, "t": 3, "r": 4}
+
+df_ent = pd.read_csv("scripts/X_proj_scaled_avec_labels_corrige_avant.csv")
+
+df_ent["label"] = df_ent["label"].map(label_mapping)
+
+X_ent = df_ent.iloc[:, :-1].values
+y_ent = df_ent["label"].values.astype(int)
+
+centroids = {}
+thresholds = {}
+
+# Coefficients spécifiques par label
+sigma_factors = {
+    "L": (5, 5, 5),
+    "T": (5, 4, 4),
+    "S": (5, 5, 5),
+    "A": (5, 4, 4),
+    "R": (5, 5, 5),
+}
+
+for label in list(Label)[:-1]:  # On exclut Label.VIDE
+    label_name = label.name
+    X_class = X_ent[y_ent == label.value]
+
+    # Calcul du centroïde
+    center = np.mean(X_class, axis=0)
+    centroids[label_name] = center
+
+    # Seuils 3-sigma (ou autres) par axe selon le label
+    factors = sigma_factors[label_name]
+    thresholds[label_name] = {
+        "axis0": factors[0] * np.std(X_class[:, 0]),
+        "axis1": factors[1] * np.std(X_class[:, 1]),
+        "axis2": factors[2] * np.std(X_class[:, 2])
+    }
+
+# Initialisation de l'état de l'automate
+etat = "1"
 try:
-    print("go")
+    sequence_labels = []
+    last_action_count = 0
+    print("Go")
     while True:
-        data = stream.read(CHUNK, exception_on_overflow=False) 
-        audio_data = np.frombuffer(data, dtype=np.int16) 
+        data = stream.read(CHUNK)
+        audio_data = np.frombuffer(data, dtype=np.int16)
 
-        # Appliquer le filtre passe-bas
-        filtered_audio = lfilter(b, a, audio_data)
+        # Ajouter au tampon
+        audio_buffer = np.concatenate((audio_buffer, audio_data))
 
-        # Calcul des MFCCs
-        block = filtered_audio.astype(np.float32) / 32768.0
-        fs = RATE
-        start += len(filtered_audio)
-        
-        mfcc = spectral.mfcc(y=block.astype(float), sr=fs, n_mfcc=13,
-                                    n_fft=min(512, len(block)), hop_length=32,
-                                    n_mels=10, fmax=fs/2, mel_basis=mel_basis)
-        mfcc_vector = mfcc.flatten()
+        while len(audio_buffer) >= int(CHUNK * taux_recouvrement):
 
-        # Ajouter à la mémoire tampon
-        mfcc_buffer.append(mfcc_vector)
+            # Parametres pour le calcul des MFCC
+            all_audio_buffer = audio_buffer[:int(CHUNK * taux_recouvrement)] 
+            audio_buffer = audio_buffer[CHUNK:] 
+            block = all_audio_buffer.astype(np.float32) / 32768.0
+            fs = RATE
+            start += len(audio_buffer)
+            
+            # Calcule des MFCC
+            mfcc = librosa.feature.mfcc(y=block.astype(float), sr=fs ,n_mfcc=8,
+                                n_fft=min(512, len(block)), win_length = n_fft, hop_length= win_length // 10,
+                                fmax=fs/2, mel_basis = mel_basis)
+            
+            mfcc_vector = mfcc.flatten()
+            mfcc_centered = mfcc_vector - mean_X[:-55]
+            mfcc_scaled = mfcc_centered / std_X[:-55] 
 
-        # Prédire quand on a accumulé assez de blocs
-        if len(mfcc_buffer) >= batch_size:
-            df_mfcc = pd.DataFrame(mfcc_buffer)
-            predictions = knn_model.predict(df_mfcc)  
-            # tab_pred.extend(predictions) # Decommenter pour l'affichage
+            # Ajouter à la mémoire tampon
+            mfcc_buffer.append(mfcc_scaled)
+            
+            # Prédire quand on a accumulé assez de blocs
+            if len(mfcc_buffer) >= batch_size:
 
-            for pred in predictions:
-                if pred != last_prediction: 
-                    # print("pred:", pred)
-                    if pred == 0 and etat != 0:  # Afficher uniquement le premier 0
-                        # print("ON")
-                        etat = 0 # etat O = ON
-                        fluid.noteon(0, midi_notes[note_id].note, midi_notes[note_id].velocity)
+                # Projeter les MFCCs sur les vecteurs propres
+                df_mfcc = pd.DataFrame(mfcc_buffer)
+                df_mfcc_pca = df_mfcc @ eigenvectors_thresholded
 
-                    elif pred == 2 and etat != 2:  # Afficher uniquement le premier 2
-                        # print("OFF")
-                        etat = 2 # etat 2 = OFF
-                        fluid.noteoff(0,  midi_notes[note_id].note)
-                        note_id += 1
+                # Predire avec le modèle KNN
+                predictions = knn_model.predict(df_mfcc_pca)
+                predictions_indices = predictions 
+                        
+                # Calcule les labels majoritaires
+                majority_label = int(np.bincount(predictions_indices).argmax())
+                predictions[:] = majority_label
 
-                    last_prediction = pred
-
-            # Réinitialiser les buffers
-            mfcc_buffer = []
+                # Calcul des etats et actions
+                etat, action = transition(etat, Label(majority_label).name.lower())
+                if action == "ON_OFF":
+                    handle_event("ON")
+                    handle_event("OFF")
+                elif action:
+                    handle_event(action)
+    
+                # Réinitialiser les buffers
+                mfcc_buffer = []
 
 except KeyboardInterrupt:
     print("\nArrêt de l'enregistrement.")
--- scripts/calcul_mfcc.py
+++ scripts/calcul_mfcc.py
@@ -2,17 +2,16 @@ import pandas as pd
 import librosa
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.decomposition import PCA
-import modif_libro.spectral as spectral
 
 ##########################################################################################
 ## CHARGEMENTS
 # Charger les marqueurs
-file_path = "audio/tatata_suite_add.txt"
+file_path = "audio/tata_lala_sans_r_parallel.txt"
+
 markers_df = pd.read_csv(file_path, sep="\t", header=None, names=["start", "end", "label"])
 
-# Charger l'audio
-audio_file = "audio/tatata_suite_add_precis.wav"
+# # Charger l'audio
+audio_file = "audio/tata_lala_sans_r_parallel.wav"
 signal, fs = librosa.load(audio_file, sr=None)
 
 ##########################################################################################
@@ -22,21 +21,22 @@ duration = len(signal) / fs
 time = np.linspace(0, duration, num=len(signal))
 
 # Parametres MFCC
-block_size = int(0.005 * fs)
+block_size = int(0.005 * fs)  # 30 ms en echantillons
+recouvrement = int(block_size/2)
 mfcc_per_block = []
 block_labels = []  
 block_start_times = []
 
 # Identifier les segments pour chaque bloc
-for i, start in enumerate(range(0, len(signal), block_size)): 
-    print(f"i: {i} / {len(signal) // block_size}", end="\r", flush=True)
+for i, start in enumerate(range(0, len(signal), recouvrement)): 
+    print(f"i: {i} / {len(signal) // recouvrement}", end="\r", flush=True)
  
     block = signal[start:start+block_size]
     block_start_time = start / fs  
     block_end_time = (start + block_size) / fs 
 
     # Trouver la classe dominante
-    segment_durations = {"t": 0, "a": 0}
+    segment_durations = {"l": 0, "a": 0, "t": 0, "r": 0}
     
 
     for _, row in markers_df.iterrows():
@@ -45,33 +45,49 @@ for i, start in enumerate(range(0, len(signal), block_size)):
         if segment_end > block_start_time and segment_start < block_end_time:
             overlap_start = max(block_start_time, segment_start)
             overlap_end = min(block_end_time, segment_end)
-            segment_durations[segment_label] += (overlap_end - overlap_start) 
+            segment_durations[segment_label] += (overlap_end - overlap_start) # duree du chevauchement
 
-    # Si le bloc chevauche plusieurs classes, on l'ignore
-    if (segment_durations["t"] > 0) and (segment_durations["a"] > 0):
-        continue  
+    # Trouver les lettres actives (durée > 0)
+    active_segments = [k for k, v in segment_durations.items() if v > 0]
 
-    # Attribuer le segment dominant
-    if segment_durations["t"] > 0:
-        block_label = "t"
-    elif segment_durations["a"] > 0:
-        block_label = "a"
-    else:
-        block_label = "s" 
+    excluded_pairs = [
+        {"l", "a"},
+        {"t", "a"},
+        {"s", "t"},
+        {"l", "r"},
+        {"a", "r"},
+        {"s", "r"},
+        {"l", "s"},
+        {"t", "r"},
+    ]
+
+    # Cas 1 — Aucun segment actif
+    if len(active_segments) == 0:
+        # print("cas1")
+        block_label = "s"
+
+    # Cas 2 — Exactement deux lettres actives, et la paire est dans les exclus
+    elif len(active_segments) == 2 and set(active_segments) in excluded_pairs:
+        continue  # On ignore ce bloc
+
+    # Cas 3 — Un seul segment actif 
+    elif len(active_segments) == 1:
+        block_label = active_segments[0]
+
 
     n_fft=min(512, block_size)
     window = np.ones(min(512, block_size))
     win_length = n_fft
 
     # Build a Mel filter
-    mel_basis = librosa.filters.mel(sr=fs, n_fft=n_fft, fmax=fs/2, n_mels=10)
+    mel_basis = librosa.filters.mel(sr=fs, n_fft=n_fft, fmax=fs/2, n_mels=40)
 
     # Calculer les MFCCs
     if len(block) == block_size:
-        mfcc = spectral.mfcc(y=block.astype(float), sr=fs ,n_mfcc=13,
-                            n_fft=min(512, len(block)), hop_length=32,
-                            n_mels=10, fmax=fs/2, mel_basis = mel_basis)
-
+        mfcc = librosa.feature.mfcc(y=block.astype(float), sr=fs ,n_mfcc=7, 
+                            n_fft=min(512, len(block)), win_length = n_fft, hop_length= win_length // 10, 
+                            fmax=fs/2, mel_basis = mel_basis)
+        
         mfcc_vector = mfcc.flatten() 
 
         mfcc_per_block.append(mfcc_vector)
@@ -87,37 +103,12 @@ df_mfcc = pd.DataFrame(mfcc_matrix)
 df_mfcc["start_time"] = block_start_times
 df_mfcc["label"] = block_labels
 
-df_mfcc.to_csv("mfcc_features.csv", index=False)
+df_mfcc.to_csv("mfcc_features_ta_la_sans_r_main_plus_court.csv", index=False) # mfcc_features_ta_la_r_avant ou mfcc_features_ta_la_r_plus_court ou  mfcc_features_ta_la_r_fonctionne
 print("MFCCs calculés et sauvegardés dans 'mfcc_features.csv'")
 
 ##########################################################################################
-# AFFICHAGE PCA
-# Verification que toutes les classes sont bien attribuees
-assert all(label in ["t", "a", "s"] for label in block_labels), "Erreur: Un bloc n'est pas classifie !"
-
-# PCA pour visualiser en 2D
-pca = PCA(n_components=2)
-mfcc_2d = pca.fit_transform(mfcc_matrix)
-
-# Associer les couleurs aux classes
-colors = {"t": "blue", "a": "green", "s": "red"}  
-class_colors = [colors[label] for label in block_labels] 
-
-plt.figure(figsize=(8, 6))
-scatter = plt.scatter(mfcc_2d[:, 0], mfcc_2d[:, 1], c=class_colors, edgecolors='k')
-plt.xlabel("Composante principale 1")
-plt.ylabel("Composante principale 2")
-plt.title("Projection des MFCCs en 2D avec Classes")
-
-# Ajouter une légende
-handles = [plt.Line2D([0], [0], marker='o', color='w', markerfacecolor=color, markersize=10) for color in colors.values()]
-plt.legend(handles, ["t", "a", "s"], title="Classes")
-
-plt.grid()
-plt.show()
-
-##########################################################################################
 # AFFICHAGE DU SIGNAL AVEC COULEURS PAR BLOCS
+colors = {"l": "blue", "a": "green", "s": "red", "t": "orange", "r": "purple"}  
 plt.figure(figsize=(12, 4))
 for i, (start_time, label) in enumerate(zip(block_start_times, block_labels)):
     color = colors[label]
